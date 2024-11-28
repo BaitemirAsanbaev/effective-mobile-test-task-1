75eb6176855b2ad763ab223cc296aa233b797cc8
BaitemirAsanbaev <baitemir1706@gmail.com>
2024-11-29 00:38:50 +0600

    logger

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const ShopRouter = require('./shop/router');
 const InventoryRouter = require('./inventory/router');
 const swaggerSpec = require('./swagger');
 const swaggerUi = require('swagger-ui-express');
+const logger = require("./logger");
+const morgan = require("morgan");
+
 dotenv.config();
 
 const app = express();
@@ -15,19 +18,28 @@ app.use(cors({
     credentials: true,
     origins: [process.env.CLIENT_URL]
 }));
+
 app.use(express.json());
+
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(morgan("combined", { stream: { write: (message) => logger.info(message.trim()) } }));
+
 app.use("/api/v1/item", ItemRouter);
 app.use("/api/v1/shop", ShopRouter);
 app.use("/api/v1/inventory", InventoryRouter);
+
+
 app.use(errorHandler);
 
-const start = async () => {
-    const port = process.env.PORT;
+const start = () => {
+    const port = process.env.PORT || 5000;
     try {
-        await app.listen(port, () => console.log("Started at port: ", port));
+        app.listen(port, () => {
+            logger.info(`Server started on port: ${port}`);
+        });
     } catch (e) {
-        console.log(e.message);        
+        logger.error(`Error starting the server: ${e.message}`);
     }
 };
 
